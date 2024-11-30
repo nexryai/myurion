db8f37e183c7f528372c584ff9c77f21d0253774
nexryai <nexryai@sda1.net>
2024-11-30 11:24:36 +0900

    Clean up

--- src/lib/server/AppController.ts
+++ src/lib/server/AppController.ts
@@ -23,7 +23,7 @@ export class AppController {
                 return "Invalid request"
             }
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+             
             // @ts-ignore
             if (code == 401) {
                 // なぜかset.status = 401が型エラーになる
