bf42593c94896f4b496010bbd74125f791a732b9
nexryai <nexryai@sda1.net>
2024-11-30 11:19:50 +0900

    Add icon selector

---
+++ src/lib/components/icons/SelectIcon.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import * as Select from "$lib/components/ui/select/index.js";
+    import { userSelectableIcons } from "./icons";
+
+    let {
+        selected = $bindable("")
+    }: {
+        selected?: string;
+    } = $props()
+
+    const icons = userSelectableIcons;
+
+    const triggerContent = $derived(
+        icons.find((icon) => icon.name === selected)?.name ?? "Select an icon"
+    );
+
+    function setHTML(node: HTMLElement, html: string) {
+        node.innerHTML = html;
+        return {
+            update(newHTML: string) {
+                if (newHTML !== html) {
+                    html = newHTML;
+                    node.innerHTML = html;
+                }
+            }
+        };
+    }
+</script>
+
+<Select.Root type="single" name="iconSelector" bind:value={selected}>
+    <Select.Trigger class="w-[180px]">
+        {#if selected}
+            <span use:setHTML={icons.find((icon) => icon.name === selected)?.svg || ""}></span>
+            <span>{triggerContent}</span>
+        {:else}
+            <span>Select an icon</span>
+        {/if}
+    </Select.Trigger>
+    <Select.Content>
+        <Select.Group>
+            <Select.GroupHeading>Icons</Select.GroupHeading>
+            {#each icons as icon}
+                <Select.Item value={icon.name} label={icon.name}>
+                    <div class="flex items-center">
+            <span
+                    class="w-5 h-5 mr-2 select-item-icon"
+                    use:setHTML={icon.svg}
+            ></span>
+                        {icon.name}
+                    </div>
+                </Select.Item>
+            {/each}
+        </Select.Group>
+    </Select.Content>
+</Select.Root>
+
+<style lang="scss">
+    :global {
+        .select-item-icon {
+            > .icon {
+                padding-bottom: 4px;
+            }
+        }
+    }
+</style>
---
+++ src/lib/components/icons/icons.ts
@@ -0,0 +1,19 @@
+interface UserSelectableIcon {
+    name: string;
+    svg: string;
+}
+
+export const userSelectableIcons: UserSelectableIcon[] = [
+    {
+        name: "Home",
+        svg: "<svg  xmlns=\"http://www.w3.org/2000/svg\"  width=\"24\"  height=\"24\"  viewBox=\"0 0 24 24\"  fill=\"none\"  stroke=\"currentColor\"  stroke-width=\"2\"  stroke-linecap=\"round\"  stroke-linejoin=\"round\"  class=\"icon icon-tabler icons-tabler-outline icon-tabler-home\"><path stroke=\"none\" d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M5 12l-2 0l9 -9l9 9l-2 0\" /><path d=\"M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7\" /><path d=\"M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6\" /></svg>"
+    },
+    {
+        name: "Briefcase",
+        svg: "<svg  xmlns=\"http://www.w3.org/2000/svg\"  width=\"24\"  height=\"24\"  viewBox=\"0 0 24 24\"  fill=\"none\"  stroke=\"currentColor\"  stroke-width=\"2\"  stroke-linecap=\"round\"  stroke-linejoin=\"round\"  class=\"icon icon-tabler icons-tabler-outline icon-tabler-briefcase\"><path stroke=\"none\" d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z\" /><path d=\"M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2\" /><path d=\"M12 12l0 .01\" /><path d=\"M3 13a20 20 0 0 0 18 0\" /></svg>"
+    },
+    {
+        name: "School",
+        svg: "<svg  xmlns=\"http://www.w3.org/2000/svg\"  width=\"24\"  height=\"24\"  viewBox=\"0 0 24 24\"  fill=\"none\"  stroke=\"currentColor\"  stroke-width=\"2\"  stroke-linecap=\"round\"  stroke-linejoin=\"round\"  class=\"icon icon-tabler icons-tabler-outline icon-tabler-school\"><path stroke=\"none\" d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M22 9l-10 -4l-10 4l10 4l10 -4v6\" /><path d=\"M6 10.6v5.4a6 3 0 0 0 12 0v-5.4\" /></svg>"
+    }
+]
