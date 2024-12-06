2a249e9c418a8b36c6978f98f329c4b8794d3c50
nexryai <nexryai@sda1.net>
2024-12-06 18:50:09 +0900

    e2e: improve tests

--- tests/e2e/basic.test.ts
+++ tests/e2e/basic.test.ts
@@ -13,7 +13,7 @@ test.describe("Before login", () => {
         expect(h1Text).toBe("Welcome back");
     });
 
-    test("パスキー（WebAuthn）でサインアップ => サインインできる", async ({ browserName, page }) => {
+    test("パスキー（WebAuthn）でサインアップ => サインインできる", async ({ browserName, page }, testInfo) => {
         test.skip(browserName !== "chromium", "This test runs only in Chromium");
         await page.goto("/");
 
@@ -40,17 +40,40 @@ test.describe("Before login", () => {
         await page.click("#sign-up-button");
         await page.fill("#name", "Test User");
         await page.click("#terms");
+        const beforeSignUpScreenshot = await page.screenshot();
+        await testInfo.attach("Before sign-up", {
+            body: beforeSignUpScreenshot,
+            contentType: "image/png",
+        });
+
         await page.click("#sign-up-submit-button");
-        await page.waitForTimeout(1000);
-        await page.screenshot({ path: "./signupResult.png" });
+        await page.waitForTimeout(10);
+        const afterSignUpScreenshot = await page.screenshot();
+        await testInfo.attach("After click sign-up button", {
+            body: afterSignUpScreenshot,
+            contentType: "image/png",
+        });
+
+        // やたら時間がかかることがある
+        await page.waitForTimeout(8000);
         await page.reload();
 
         // サインインする
         await page.click("#button-sign-in-with-passkey");
+        await page.waitForTimeout(500);
+        const afterSignInScreenshot = await page.screenshot();
+        await testInfo.attach("After click sign-in button", {
+            body: afterSignInScreenshot,
+            contentType: "image/png",
+        });
 
         // 自動的にリダイレクトされるので待つ
-        await page.waitForTimeout(4000);
-        await page.screenshot({ path: "./afterLogin.png" });
+        await page.waitForTimeout(8000);
+        const appScreenshot = await page.screenshot();
+        await testInfo.attach("After sign-in", {
+            body: appScreenshot,
+            contentType: "image/png",
+        });
 
         // 成功していればサイドバーがあるはず
         expect(await page.locator("#myurion-app-sidebar").count()).toBeGreaterThan(0);
