a130fd2c784285a4d320634372492a519a7bdb3e
nexryai <nexryai@sda1.net>
2024-12-11 11:40:07 +0900

    Remove an unused import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,6 @@
     import FatalErrorDialog from "$lib/components/error/FatalErrorDialog.svelte";
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import { type User } from "@prisma/client";
-    import { callApi } from "$lib/browser/api";
 
     // states
     let tokenExpired = $state(false);
