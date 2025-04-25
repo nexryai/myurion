ca0fc7df9452bc7a12c8f0cf4ceccac9063b8dd6
nexryai <nexryai@sda1.net>
2025-04-25 07:46:14 +0000

    CI: Playwrightで本番ビルドを使うように

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: "http://localhost:5173",
+        baseURL: "http://localhost:3000",
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
@@ -72,7 +72,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: "pnpm run dev",
+        command: "pnpm run docker:start",
         url: "http://localhost:5173",
         reuseExistingServer: !process.env.CI,
     },
